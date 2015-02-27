a141531addf15108531abae2abbdad9a725b668c
Sue Shebel <shebel@fastmail.net>
2015-02-26 21:47:04 -0500

    add documentation

--- movie.py
+++ movie.py
@@ -1,7 +1,16 @@
 import webbrowser
 
 class Movie:
+    """ Class to hold movie information for use in creating a web page of favorite movies.
 
+        Attributes:
+            name = movie name
+            poster = movie poster image url
+            trailer = url youtube movie trailer
+            rating = movie rating, must be one of the ratings specifed in the class ratings dictionary
+            year = year movie released
+            category = movie category (drama, comedy, etc.)
+     """       
     ratings = {'G':'<span style="color:green; font-family:Cooper Black">G</span>',
                'PG':'<span style="color:orange; font-family:Cooper Black">PG</span>',
                'PG-13':'<span style="color:purple; font-family:Cooper Black">PG-13</span>',
@@ -10,9 +19,9 @@ class Movie:
         self.title = name
         self.poster_image_url = poster
         self.trailer_youtube_url = trailer
-        self.rating = rating; # must be one of the ratings in the ratings dictionary
+        self.rating = rating; 
         self.year = year
-        self.category = category # Drama, Comedy, etc.
+        self.category = category 
 
     def show_trailer(self):
         webbrowser.open(self.trailer)
