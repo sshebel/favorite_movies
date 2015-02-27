9cd1bcb38a1afea048a76cf13a00a88e84ecf33b
Sue Shebel <shebel@fastmail.net>
2015-02-26 21:47:28 -0500

    add documentation

--- mymovies.py
+++ mymovies.py
@@ -2,7 +2,7 @@ from movie import Movie
 import fresh_tomatoes
 
 # Create an html page containing information about my favorite movies and access to the trailers.
-
+# Create a Movie class instance for each movie to be displayed
 knights_tale=Movie("A Knight's Tale",
                          "http://upload.wikimedia.org/wikipedia/en/a/a6/AKnightsTale.jpg",
                          "https://www.youtube.com/watch?v=KXvp57cURpU","PG-13","2001","Drama/Action")
@@ -15,6 +15,8 @@ gladiator=Movie("Gladiator",
                 "http://posterpress.us/uploads/g/gladiator_2000.jpg",
                 "www.youtube.com/watch?v=IvTT29cavKo","R","2000","Drama/Action")
 
+# create list of movies to pass to open_movies_page function
 movies=[gladiator,knights_tale, sound_of_music]
 
+# create the web page
 fresh_tomatoes.open_movies_page(movies)
